137a05b2e601eecc6548a58dbb2cb94f47efa20c
John Rinehart <john.rinehart@ardanlabs.com>
2021-09-29 15:09:55 +0300

    fix(/flake.nix): change built package to be the HTTP server instead of GCC hello

--- flake.nix
+++ flake.nix
@@ -8,7 +8,28 @@
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in
     {
-      defaultPackage.x86_64-linux = pkgs.hello;
+      defaultPackage.x86_64-linux =
+        let
+          lib = pkgs.lib;
+        in
+        pkgs.buildGoModule {
+          pname = "ArdanLabsNixDemo";
+          version = "0.0.2";
+
+          modSha256 = lib.fakeSha256;
+          vendorSha256 = null;
+
+          src = ./.;
+
+          meta = {
+            description = "World's simplest HTTP server to demonstrate how to package Go applications with Nix";
+            homepage = "https://github.com/johnrichardrinehart/ArdanLabsNixDemo";
+            license = lib.licenses.mit;
+            maintainers = [ "johnrichardrinehart" ];
+            platforms = lib.platforms.linux ++ lib.platforms.darwin;
+          };
+        };
+
       devShell = import ./shell.nix { inherit pkgs; };
     };
 }

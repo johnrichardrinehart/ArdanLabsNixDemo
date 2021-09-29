cb20d061e90625f4c78b9bb52b27ccf7fd835fcc
John Rinehart <john.rinehart@ardanlabs.com>
2021-09-29 15:17:50 +0300

    feat(flake.nix): use numtide/flake-utils to generate a derivation for every supported system

--- flake.nix
+++ flake.nix
@@ -1,35 +1,39 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/31ffc50c";
+    flake-utils.url = "github:numtide/flake-utils/7e5bf3925";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in
-    {
-      defaultPackage.x86_64-linux =
-        let
-          lib = pkgs.lib;
-        in
-        pkgs.buildGoModule {
-          pname = "ArdanLabsNixDemo";
-          version = "0.0.2";
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  rec {
+    packages = flake-utils.lib.flattenTree {
+      demoServer =  let lib = pkgs.lib; in
+      pkgs.buildGoModule {
+        pname = "ArdanLabsNixDemo";
+        version = "0.0.2";
 
-          modSha256 = lib.fakeSha256;
-          vendorSha256 = null;
+        modSha256 = lib.fakeSha256;
+        vendorSha256 = null;
 
-          src = ./.;
+        src = ./.;
 
-          meta = {
-            description = "World's simplest HTTP server to demonstrate how to package Go applications with Nix";
-            homepage = "https://github.com/johnrichardrinehart/ArdanLabsNixDemo";
-            license = lib.licenses.mit;
-            maintainers = [ "johnrichardrinehart" ];
-            platforms = lib.platforms.linux ++ lib.platforms.darwin;
-          };
+        meta = {
+          description = "World's simplest HTTP server to demonstrate how to package Go applications with Nix";
+          homepage = "https://github.com/johnrichardrinehart/ArdanLabsNixDemo";
+          license = lib.licenses.mit;
+          maintainers = [ "johnrichardrinehart" ];
+          platforms = lib.platforms.linux ++ lib.platforms.darwin;
         };
-
-      devShell = import ./shell.nix { inherit pkgs; };
+      };
     };
+
+    defaultPackage = packages.demoServer;
+
+    devShell = import ./shell.nix { inherit pkgs; };
+  }
+  );
 }
